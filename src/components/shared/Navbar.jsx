38cd97699eb1378a5a0eafa51327011a34732088
sauravprajapatiii <sauravprajapati1411@gmail.com>
2026-04-11 10:45:32 +0530

    feat: initial commit

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div className="bg-white ">
+      <div className="flex justify-between items-center mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Job <span className="text-[#F83002]">Portal</span>
+          </h1>
+        </div>
+        <div>
+          <ul className="flex gap-5 font-medium items-center">
+            <li>
+              <Link>Home</Link>
+            </li>
+            <li>
+              <Link>About</Link>
+            </li>
+            <li>
+              <Link>Browse</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
