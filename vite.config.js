4b91320b825cbbb2d473b425f9125e35037b2c30
sauravprajapatiii <sauravprajapati1411@gmail.com>
2026-04-19 16:30:12 +0530

    just

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "jobhunt-backend-rouge.vercel.app",
         changeOrigin: true,
         secure: false,
       },
